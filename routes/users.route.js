ab0aed11b24ea3c5e60af9e94530bdfb92a9d086
Shelly Agarwal <shelly@gkmit.co>
2024-03-10 22:33:59 +0530

    chore: fix routes

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const userController = require('../controllers/user.controller.js');
 const userValidator = require('../validators/user.validator.js');
 
-router.post('/sign-up', userValidator.userSignUp, userController.signUp);
-router.post('/forget-password', userValidator.userForgetPassword, userController.forgetPassword);
-router.get('/sign-in', userValidator.userSignIn, userController.signIn);
+router.post('/register', userValidator.userSignUp, userController.signUp);
+router.post('/password/reset', userValidator.userForgetPassword, userController.forgetPassword);
+router.get('/login', userValidator.userSignIn, userController.signIn);
 router.put('/change-password/:id', userValidator.userChangePasswordId, userValidator.userChangePassword, userController.changePassword);
 
 module.exports = router;
